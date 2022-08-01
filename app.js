7c7c43455bb014f151e8ca4d710194d666de1cf5
SKS2502 <sachin2502singh@gmail.com>
2022-08-01 15:41:46 +0530

    Here the website

---
+++ app.js
@@ -0,0 +1,27 @@
+/*const hamburger_menu=document.querySelector(".hamburger-menu");
+const navbar = document.querySelector(".header nav");
+const links = document.querySelectorAll(".links a");
+
+
+function closeMenu(){
+    navbar.classList.remove("open");
+    document.body.classList.remove("stop-scrolling");
+}
+
+
+hamburger_menu.addEventListener("click",() => {
+    if(!navbar.classList.contains("open")) {
+        document.body.classList.add("stop-scrolling");
+        navbar.classList.add("open");
+    }else{
+        closeMenu();
+    }
+});*/
+
+
+/* ===========  typing animation  ===========================*/
+
+
+
+
+
